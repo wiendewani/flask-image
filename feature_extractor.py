7778aea3debe317d41dae0dad08e8fea5ec34049
Wien Nurul Dewani <wiennuruldewani@gmail.com>
2021-12-13 11:40:28 +0700

    baru

--- feature_extractor.py
+++ feature_extractor.py
@@ -7,7 +7,7 @@ class FeatureExtractor:
         self.model = model
 
     def extract(self, img):
-        img = img.resize((150, 150))
+        img = img.resize((256, 384))
         img = img.convert('RGB')
 
         x = img_to_array(img)
